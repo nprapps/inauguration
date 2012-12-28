74c8e8565a2f485997e89e5050ae6a2015c017cf
Christopher Groskopf <staringmonkey@gmail.com>
2012-12-28 17:39:04 -0600

    More testing experiments.

--- www/test/spec/chat.js
+++ www/test/spec/chat.js
@@ -11,6 +11,9 @@ describe('$.livechat', function() {
         // Fake Timers
         this.timers = sinon.useFakeTimers();
 
+        // JSON Fixtures
+        jasmine.getJSONFixtures().fixturesPath = '/test/fixtures'
+
         // Sandbox
         sandbox({ id: 'live-chat' });
     });
@@ -20,14 +23,14 @@ describe('$.livechat', function() {
         this.xhr.restore();
     });
 
-    xit('should take options', function() {
+    it('should take options', function() {
         var $livechat = $('#live-chat').livechat({
             chat_id: '123456',
             chat_token: 'ABCDEFG',
             update_interval: 123,
             alert_interval: 456,
             read_only: true,
-            scribble_host: 'test.com'
+            scribble_host: window.location.host
         });
 
         expect($livechat.settings.chat_id).toBe('123456');
@@ -35,7 +38,7 @@ describe('$.livechat', function() {
         expect($livechat.settings.update_interval).toBe(123);
         expect($livechat.settings.alert_interval).toBe(456);
         expect($livechat.settings.read_only).toBe(true);
-        expect($livechat.settings.scribble_host).toBe('test.com');
+        expect($livechat.settings.scribble_host).toBe(window.location.host);
     });
 
     it('should update automatically', function() {
@@ -53,16 +56,23 @@ describe('$.livechat', function() {
         expect(this.requests.length).toBe(2);
     });
 
-    xdescribe('update_live_chat', function() {
+    describe('update_live_chat', function() {
         beforeEach(function() {
             this.$livechat = $('#live-chat').livechat({
                 chat_id: '74796',
                 chat_token: 'FtP7wRfX',
+                update_interval: 100,
+                scribble_host: window.location.host 
             });
+
+            //this.requests[0].respond(200, { 'Content-Type': 'application/json' }, getJSONFixture('chat_posts_base.json'));
         });
 
-        it('should fetch the latest posts', function() {
-            // TODO
+        xit('should fetch the latest posts', function() {
+            //expect(this.requests.length).toBe(0);
+            //this.timers.tick(150);
+            //expect(this.requests.length).toBe(1);
+
         });
     });
 });
