930329c7cb350e52128b6d8fec948cf71c06453a
Jeremy Bowers <jeremyjbowers@gmail.com>
2013-01-11 16:39:52 -0500

    Adds a first pass at hiding/showing the live chat bits.

--- www/js/app.js
+++ www/js/app.js
@@ -1,12 +1,37 @@
-$('#live-chat').livechat({
-    chat_id: '74796',
-    chat_token: 'FtP7wRfX',
-    update_interval: 10000,
-    alert_interval: 500,
-    read_only: false
-});
+/*
+* Prepare the live chat widget.
+*/
 $('#live-chat-widget').livechatwidget({
     chat_id: '74796',
     chat_token: 'FtP7wRfX',
     update_interval: 10000
+});
+
+$('button.live-chat-toggle').on('click', function(){
+    /*
+    * Toggle the live chat window.
+    * Exciting!
+    */
+    var $livechat = $('#live-chat');
+    $livechat.toggle();
+    if ($livechat.is(':visible')){
+        /*
+        * If the chat is visible, load up the module and set up the button.
+        */
+        $('button.live-chat-toggle').html('Turn off live chat.');
+        $('#live-chat').livechat({
+            chat_id: '74796',
+            chat_token: 'FtP7wRfX',
+            update_interval: 10000,
+            alert_interval: 500,
+            read_only: false
+        });
+    } else {
+        /*
+        * Otherwise, kill the module and set up the button.
+        */
+        $('button.live-chat-toggle').html('Turn on live chat.');
+        $('#live-chat').livechat = null;
+    }
+
 });
