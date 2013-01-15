74e254ab12697e8accc840a0fa70ad63c3f4eb50
Alyson Hurt <ahurt@npr.org>
2013-01-14 22:59:39 -0500

    tabbify it

--- www/js/app.js
+++ www/js/app.js
@@ -9,11 +9,12 @@ $(function(){
     var CHAT_UPDATE_INTERVAL = 5000;
 
     // Caching some DOM objects
-    var $button = $('button.live-chat-toggle');
     var $live = $('#live-chat');
+    var $live_tab = $('#chat-toggle');
     var $widget = $('#live-chat-widget');
     var $mrprez = $('#mr-president');
-
+    var $mrpres_tab = $('#mr-president-toggle');
+    
     /*
     * Initializes the live chat widget.
     */
@@ -29,34 +30,38 @@ $(function(){
     /*
     * Toggle all of the things.
     */
-    $button.on('click', function(){
+    $mrpres_tab.on('click', function() {
+    	$live.hide();
+    	$widget.show();
+    	$mrprez.show();
+    	
+    	$(this).addClass('selected');
+    	$live_tab.removeClass('selected');
 
-        $live.toggle();
-        $widget.toggle();
-        $mrprez.toggle();
-
-        if ($live.is(':visible')){
-            /*
-            * If the live chat is alive, set up the live chat JS and unregister the widget.
-            */
-            $button.html('Turn off live chat.');
-            $widget.livechatwidget = null;
-            $live.livechat({
-                chat_id: CHAT_ID,
-                chat_token: CHAT_TOKEN,
-                update_interval: CHAT_UPDATE_INTERVAL,
-                alert_interval: 500,
-                read_only: false
-            });
-        } else {
-            /*
-            * Otherwise, kill the live chat module and set up the widget.
-            */
-            $button.html('Turn on live chat.');
-            $live.livechat = null;
-            init_live_widget();
-        }
+		/*
+		* Kill the live chat module and set up the widget.
+		*/
+		$live.livechat = null;
+		init_live_widget();
     });
-//    $button.trigger('click');
+    $live_tab.on('click', function() {
+    	$live.show();
+    	$widget.hide();
+    	$mrprez.hide();
 
+    	$(this).addClass('selected');
+    	$mrpres_tab.removeClass('selected');
+
+		/*
+		* If the live chat is alive, set up the live chat JS and unregister the widget.
+		*/
+		$widget.livechatwidget = null;
+		$live.livechat({
+			chat_id: CHAT_ID,
+			chat_token: CHAT_TOKEN,
+			update_interval: CHAT_UPDATE_INTERVAL,
+			alert_interval: 500,
+			read_only: false
+		});
+    });
 });
