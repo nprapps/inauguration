21cdcfcd159be2922a03aec5541f5da83b9fc432
Christopher Groskopf <staringmonkey@gmail.com>
2013-01-16 17:15:48 -0600

    Photo navigation links. Closes #91.

--- www/js/app-photo-feed.js
+++ www/js/app-photo-feed.js
@@ -116,11 +116,22 @@ $(document).ready(function() {
          * Render the photo modal.
          */
         var photo_id = $(this).data('photo');
+        var data = {
+            'photo': photos[photo_id],
+            'previous': $(this).prev('.photo-link').data('photo'),
+            'next': $(this).next('.photo-link').data('photo')
+        };
 
-        var modal_html = JST.photo_modal({ photo: photos[photo_id] });
+        var modal_html = JST.photo_modal(data);
         $photo_modal.html(modal_html);
 
         $photo_modal.modal('show');
+    }
+
+    function modal_link_clicked() {
+        var photo_id = $(this).data('photo');
+
+        render_modal.call($('.photo-link[data-photo="' + photo_id + '"]'));
     }
 
     function init() {
@@ -141,6 +152,7 @@ $(document).ready(function() {
         });
     
         $photo_container.delegate('.photo-link', 'click', render_modal);
+        $photo_modal.delegate('.navigate', 'click', modal_link_clicked);
 
         var lazy_fetch_next = _.debounce(fetch_next, 300);
         $('#photo-feed .wrapper').scroll(lazy_fetch_next);
