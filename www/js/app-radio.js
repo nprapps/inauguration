af58f2cced350e52e44b571899da5343e36e870e
Alyson Hurt <ahurt@npr.org>
2013-01-17 22:59:35 -0500

    finish styling player and page topper. closes #146

--- www/js/app-radio.js
+++ www/js/app-radio.js
@@ -1,5 +1,14 @@
 $(function(){
     var POLLING_INTERVAL = 60000;
+
+    var $b = $('body');
+    var $radio = $('#radio');
+    var $radio_control = $('#radio-htmlcontrol')
+    var $radio_feedback = $('#radio-feedback-message');
+    var $radio_prompt = $('#radio-prompt');
+    var $radio_stream = $('#radio-htmlstream');
+    var $radio_title = $('#radio-title');
+    var $coming_message = $('#comingsoon-message');
     
     function playStream(flashStreamer,flashFile,htmlUrl,title,prompt,feedback){
         play(true,flashStreamer,flashFile,htmlUrl,title,prompt,feedback);
@@ -44,16 +53,19 @@ $(function(){
 			},
 			events: {
 				onBuffer: function () {
-                    $('#radio').addClass('buffering');
-                    $('#radio').removeClass('playing');
+					console.log('buffering')
+                    $radio.addClass('buffering');
+                    $radio.removeClass('playing');
 				},
 				onPlay: function () {
-                    $('#radio').removeClass('buffering');
-                    $('#radio').addClass('playing');
+					console.log('playing')
+                    $radio.removeClass('buffering');
+                    $radio.addClass('playing');
 				},
 				onPause: function() {
-                    $('#radio').removeClass('buffering');
-                    $('#radio').removeClass('playing');
+					console.log('paused')
+                    $radio.removeClass('buffering');
+                    $radio.removeClass('playing');
 				}
 			}
         }
@@ -67,53 +79,64 @@ $(function(){
     }
 
     function setupHtmlPlayer(url){
-        $('#radio-htmlcontrol').show();
-        $('#radio-htmlstream')[0].src = url;
+        $radio_control.show();
+        $radio_stream[0].src = url;
     }
 
     function setupAudioInterface(title,prompt,feedback){
         //just in case we're switching to a new stream
-        $('#radio').removeClass('buffering');
-        $('#radio').removeClass('playing');
+        $radio.removeClass('buffering');
+        $radio.removeClass('playing');
     
-        $("#radio-title").text(title);
-        $("#radio-prompt").text(prompt);
-        $("#radio-feedback #radio-feedback-message").text(feedback);
+        $radio_title.text(title);
+        $radio_prompt.text(prompt);
+        $radio_feedback.text(feedback);
     }
     
     //interactivity for the html player
-    $('#radio-htmlcontrol').click(function(){
-        var player = $('#radio-htmlstream')[0];
+    $radio_control.click(function(){
+        var player = $radio_stream[0];
         if(player.paused) {
             player.play();
-            $('#radio').addClass('playing');
+            $radio.addClass('playing');
         } else {
             player.pause();
-            $('#radio').removeClass('playing');
+            $radio.removeClass('playing');
         }
     });
     
-    var oldStatus = {};
+    var old_status = {};
     
     function update_radio(){
         $.getJSON('live-data/radio.json?t=' + (new Date()).getTime(), function(status) {
             //check if the status has changed
-            if(!_.isEqual(oldStatus, status)) {
-                oldStatus = status;
+            if(!_.isEqual(old_status, status)) {
+                old_status = status;
                 if(status['audio'] == 'true') {
                     if(status['streaming'] == 'true') {
                         playStream(status['flashStreamer'],status['flashFile'],status['htmlUrl'],status['title'],status['prompt'],status['feedback']); 
-	                    $("body").removeClass("comingsoon").addClass("not-coming");
+	                    $b.removeClass("comingsoon").addClass("not-coming");
                     } else {
                         playFile(status['url'],status['title'],status['prompt'],status['feedback']); 
-						$("#comingsoon-message").html(status['message']);
-	                    $("body").removeClass("not-coming").addClass("comingsoon");
+						$coming_message.html(status['message']);
+	                    $b.removeClass("not-coming").addClass("comingsoon");
                     }
-                    $("body").removeClass("no-audio").addClass("audio");
+                    $b.removeClass("no-audio").addClass("audio");
                 } else {
-                    $("#comingsoon-message").html(status['message']);
-                    $("body").removeClass("audio").addClass("no-audio");
-                    $("body").removeClass("not-coming").addClass("comingsoon");
+                    $coming_message.html(status['message']);
+                    $b.removeClass("audio").addClass("no-audio");
+                    $b.removeClass("not-coming").addClass("comingsoon");
+                }
+                switch(status['id']) {
+                	case 'pregame':
+                		$b.removeClass('live').removeClass('off').addClass('pregame');
+                		break;
+                	case 'live':
+                		$b.removeClass('pregame').removeClass('off').addClass('live');
+                		break;
+                	case 'off':
+                		$b.removeClass('pregame').removeClass('live').addClass('off');
+                		break;
                 }
             }
         });
