4ca39a0c92a6b1e10b99674046911404a1942783
Katie Zhu <kzhu91@gmail.com>
2013-02-19 15:41:47 -0500

    Scraper for Scribble chat archives.

---
+++ scribble_scraper.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import json
+import requests
+import os
+import urlparse
+
+import boto
+
+event_id = '78919'
+current_page = '0'
+url = 'http://apiv1.scribblelive.com/event/%s/page/%s/?Token=FtP7wRfX&format=json'
+
+r = requests.get(url % (event_id, current_page))
+json_data = r.json
+max_pages = json_data['Pages']
+pages = range(0, max_pages)
+
+output = {}
+
+# Chat data
+output['Id'] = json_data['Id']
+output['Title'] = json_data['Title']
+output['Description'] = json_data['Description']
+output['IsLive'] = json_data['IsLive']
+output['IsCommenting'] = json_data['IsCommenting']
+output['IsModerated'] = json_data['IsModerated']
+
+# Chat posts
+output['Posts'] = json_data['Posts']
+
+for page in pages:
+  r = requests.get(url % (event_id, page))
+  output['Posts'] += r.json['Posts']
+
+filename ='www/live-data/chat-archive.json'
+print "Writing JSON file to %s." % filename
+
+json_output = json.dumps(output)
+
+with open(filename, 'w') as f:
+  f.write(json_output)
+
+print "JSON written."
